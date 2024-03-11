0c57acc2c521a37bdc7da4e059b44a29a595747c
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-11 13:18:06 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width" /> <!-- SETTING DISPLAY  AS ACCORDING TO WIDTH OF USER DESKTOP-->
+    <link rel="stylesheet" href="/CSS/style.css" /> <!--CALLING CSS FILE -->
+    <link rel="shortcut icon" href="/IMAGES/log.jpg" type="image/x-icon" /><!--SETUP OF LOG ICON -->
+    <title>BAAHUBALI MOVIE POSTER</title>
+</head>
+
+<body><!-- BODY START WITH CHANGES SPECIFY IN  EXTERNAL CSS -->
+    <!--production logo image -->
+    <img src="/IMAGES/production@logo.jpg" height="50px" width="100px " alt="logo" style="float:left;" />&nbsp;&nbsp;
+
+    <header>
+        <!-- implementing header with editing done in css-->
+        <p>KARAN JOHAR and AA FILMS presents
+            &nbsp;&nbsp;&nbsp;&nbsp;<!-- adding 4 bit space after production log image   -->
+            <img src="/IMAGES/works@logo.jpg" height="50px" width="100px " alt="logo" style="float: right;" />
+        </p>
+    </header> <!-- close of card -->
+    <div class="postername"><!-- calling postername from stylesheet to mention name of movie-->
+        <p>
+
+        <h3>S&nbsp;S &nbsp; RAJAMOULI'S</h3>
+        <cite style="font-size: 100px;"> ba&#771;hubali</cite>
+        <h4>-&nbsp;&nbsp;THE BEGINNING&nbsp;&nbsp;-</h4>
+
+        </p>
+    </div>
+    <div class="posterspace"></div><!--additional space-->
+    <div class="posterspace"></div>
+    <br><br><br><br><!--adding four line space-->
+
+
+    <!-- footer with inline styling -->
+    <footer style="float: inline-start;text-align: center;">
+
+        <!-- calling posterbottom for giving production and team names with copyright and link to movie -->
+        <div class="posterbottom">
+            <p style="font-size:25px;font-family: Arial, Helvetica, sans-serif;color: rgb(210, 173, 86);">
+                <strong>10<sup>th</sup> July</strong><!--releasing date-->
+            </p>
+            <!--names of team member-->
+            <p>K RAGHAVENDRA RAO&nbsp;&nbsp; SHOBU YARLAGADDA &nbsp;&nbsp;PRASAD DEVINLNI<br />
+                V VILAYENDRA PRASAD &nbsp;&nbsp; M M KREEM<br />
+                K K SENTHIL KUMAR &nbsp;&nbsp;SABU CYRIL&nbsp;&nbsp;V SRINIVAS MOHAN&nbsp;&nbsp;KOTAGIRI VENKATESWARA
+                RAO &nbsp;&nbsp;M M SRIVILLI<br />
+                PETER HEIN &nbsp;&nbsp;P M SATHEESH&nbsp;&nbsp;RAMA RAJAMOULI&nbsp;&nbsp;PRASHANTI TIPIRNENI<br />
+                <!--link of movie-->
+                <a href="https://www.youtube.com/watch?v=tN7Uym8Bzjo" title="link for movie" target="_blank"
+                    style="text-decoration: none;color: white;">BAAHUBALI.COM
+                    &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--spacing by bits -->
+                    <img src="/IMAGES/media@link.jpg" alt="social media pic " /></a>&nbsp;&nbsp;#BAAHUBALIMOVIE<br />
+                <small>TRAMMARK &#169;2015 AJKA MLDLIAWORK PROPERTY. ALL RIGHT RESERVED. PROMOTIONAL USE ONLY
+                </small>
+
+            </p>
+        </div><!--posterbottom close -->
+    </footer>
+
+</body>
+<!--body close -->
+
+</html>
