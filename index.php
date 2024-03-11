ca9e6e2bda705214da484aba6c6481a04c388440
SUKHWINDERKAU <156840502+SUKHWINDERKAU@users.noreply.github.com>
2024-03-11 13:19:19 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,20 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width" /> <!-- SETTING DISPLAY  AS ACCORDING TO WIDTH OF USER DESKTOP-->
-    <link rel="stylesheet" href="/CSS/style.css" /> <!--CALLING CSS FILE -->
-    <link rel="shortcut icon" href="/IMAGES/log.jpg" type="image/x-icon" /><!--SETUP OF LOG ICON -->
+    <link rel="stylesheet" href="style.css" /> <!--CALLING CSS FILE -->
+    <link rel="shortcut icon" href="log.jpg" type="image/x-icon" /><!--SETUP OF LOG ICON -->
     <title>BAAHUBALI MOVIE POSTER</title>
 </head>
 
 <body><!-- BODY START WITH CHANGES SPECIFY IN  EXTERNAL CSS -->
     <!--production logo image -->
-    <img src="/IMAGES/production@logo.jpg" height="50px" width="100px " alt="logo" style="float:left;" />&nbsp;&nbsp;
+    <img src="production@logo.jpg" height="50px" width="100px " alt="logo" style="float:left;" />&nbsp;&nbsp;
 
     <header>
         <!-- implementing header with editing done in css-->
         <p>KARAN JOHAR and AA FILMS presents
             &nbsp;&nbsp;&nbsp;&nbsp;<!-- adding 4 bit space after production log image   -->
-            <img src="/IMAGES/works@logo.jpg" height="50px" width="100px " alt="logo" style="float: right;" />
+            <img src="works@logo.jpg" height="50px" width="100px " alt="logo" style="float: right;" />
         </p>
     </header> <!-- close of card -->
     <div class="postername"><!-- calling postername from stylesheet to mention name of movie-->
@@ -52,7 +52,7 @@
                 <a href="https://www.youtube.com/watch?v=tN7Uym8Bzjo" title="link for movie" target="_blank"
                     style="text-decoration: none;color: white;">BAAHUBALI.COM
                     &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--spacing by bits -->
-                    <img src="/IMAGES/media@link.jpg" alt="social media pic " /></a>&nbsp;&nbsp;#BAAHUBALIMOVIE<br />
+                    <img src="media@link.jpg" alt="social media pic " /></a>&nbsp;&nbsp;#BAAHUBALIMOVIE<br />
                 <small>TRAMMARK &#169;2015 AJKA MLDLIAWORK PROPERTY. ALL RIGHT RESERVED. PROMOTIONAL USE ONLY
                 </small>
 
